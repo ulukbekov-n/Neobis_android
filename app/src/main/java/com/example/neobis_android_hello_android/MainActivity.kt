9cc62380115a7dd885c062fa7f7ee803bcdcd893
Ulukbekov Naken <ulukbekov_n@auca.kg>
2023-04-06 03:13:21 +0600

    Finished the first project

--- app/src/main/java/com/example/neobis_android_hello_android/MainActivity.kt
+++ app/src/main/java/com/example/neobis_android_hello_android/MainActivity.kt
@@ -28,8 +28,8 @@ class MainActivity : AppCompatActivity() {
         val randomInt = java.util.Random().nextInt(6)+1
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
+            2 -> R.drawable.dice_2
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6 }
